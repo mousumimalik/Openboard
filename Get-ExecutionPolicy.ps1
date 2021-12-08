92ae17b8770c5f1e82a8d56de15ceecaf6b5b3bb
Mousumi <mousumimalik445@gmail.com>
2021-12-08 14:51:04 +0530

    UPDATE

--- Get-ExecutionPolicy.ps1
+++ Get-ExecutionPolicy.ps1
@@ -7,3 +7,8 @@ Get-ExecutionPolicy
 # Should get 'Unrestricted'
 
 nodemon filename.js
+
+# If error
+heroku login
+# After login | paster error with appname
+heroku logs --tail --app openboard-application
